d68e6a598287cda5d168a326ca56bff9b2153308
Lethal friend <12736763+lethal_friend@user.noreply.gitee.com>
2024-09-21 13:45:57 +0800

    完成前端基础框架外观

---
+++ front/src/views/admin/IndexView.vue
@@ -0,0 +1,96 @@
+<template>
+  <el-container>
+    <el-header height="60px" style="border-radius: 20px;margin: 10px;margin-bottom: 0px;">
+      <el-menu
+          :default-active="1"
+          class="el-menu-demo"
+          mode="horizontal"
+          background-color="#545c64"
+          text-color="#fff"
+          :ellipsis="false"
+          active-text-color="#ffd04b"
+          style="border-radius: 20px;"
+      >
+        <el-menu-item index="1">在线商城</el-menu-item>
+        <el-sub-menu index="2">
+          <template #title>Admin</template>
+          <el-menu-item index="2-1">个人中心</el-menu-item>
+          <el-menu-item index="2-2">修改密码</el-menu-item>
+          <el-menu-item index="2-3">退出登录</el-menu-item>
+        </el-sub-menu>
+      </el-menu>
+    </el-header>
+    <el-container>
+      <el-aside width="300px" style="border-radius: 20px;margin: 10px;">
+        <el-menu
+            active-text-color="#ffd04b"
+            background-color="#545c64"
+            class="el-menu-vertical-demo"
+            default-active="2"
+            text-color="#fff"
+        >
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon>
+                <location/>
+              </el-icon>
+              <span>管理员管理</span>
+            </template>
+            <el-menu-item index="1-1">管理员列表</el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="2">
+            <template #title>
+              <el-icon>
+                <location/>
+              </el-icon>
+              <span>商品分类管理</span>
+            </template>
+            <el-menu-item index="1-2">分类列表</el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="3">
+            <template #title>
+              <el-icon>
+                <location/>
+              </el-icon>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="1-3">商品列表</el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="4">
+            <template #title>
+              <el-icon>
+                <location/>
+              </el-icon>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-4">用户列表</el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="5">
+            <template #title>
+              <el-icon>
+                <location/>
+              </el-icon>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="1-5">订单列表</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.el-menu--horizontal > .el-menu-item:nth-child(1) {
+  margin-right: auto;
+}
+
+.el-header {
+  padding: 0px;
+}
+</style>
