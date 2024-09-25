e873d1519d56da137751db981fff18385ea906be
Lethal friend <12736763+lethal_friend@user.noreply.gitee.com>
2024-09-25 22:44:24 +0800

    完成用户后端controller层并测试

--- user_service/src/main/java/com/st/mall/userservice/controller/UserController.java
+++ user_service/src/main/java/com/st/mall/userservice/controller/UserController.java
@@ -1,17 +1,50 @@
 package com.st.mall.userservice.controller;
 
+import com.github.pagehelper.PageInfo;
 import com.st.mall.common.bean.RespBean;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.st.mall.common.bean.User;
+import com.st.mall.common.exception.StException;
+import com.st.mall.common.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @GetMapping("/hello")
-    public RespBean hello() {
-        return RespBean.ok("hello");
+    @Autowired
+    private UserService userService;
+    //添加
+    @PostMapping
+    public RespBean insert(@RequestBody User user) throws StException {
+        userService.insert(user);
+        return RespBean.ok("添加成功");
+    }
+    //修改
+    @PutMapping
+    public RespBean update(@RequestBody User user) throws StException {
+        userService.update(user);
+        return RespBean.ok("修改成功");
+    }
+
+    @DeleteMapping("/{id}")
+    public RespBean delete(@PathVariable("id") Integer id) throws StException {
+        userService.delete(id);
+        return RespBean.ok("删除成功");
+    }
+    //搜索
+    @GetMapping
+    public RespBean selectByPage(User condition, Integer pageNum, Integer pageSize) {
+        pageNum = pageNum == null ? 1 : pageNum;
+        pageSize = pageSize == null ? 5 : pageSize;
+
+        PageInfo<User> pageInfo = userService.selectByCondition(condition, pageNum, pageSize);
+        return RespBean.ok("查询成功", pageInfo);
+    }
+    //根据id查询
+    @GetMapping("/{id}")
+    public RespBean selectById(@PathVariable("id") Integer id) {
+        User user = userService.selectById(id);
+        return RespBean.ok("", user);
     }
 }
